6be79726b2dae759098209164d08a72e50c56eee
gargAbhinav4699 <34428698+gargAbhinav4699@users.noreply.github.com>
2020-09-10 23:55:05 -0400

    Added the functionality to use classname

--- src/components/Stat Card/Stat Card.js
+++ src/components/Stat Card/Stat Card.js
@@ -18,26 +18,28 @@ import './StatCard.scss';
 function StatCard(props) {
   let [scrolled, setScrolled] = useState(0);
   return (
-    <div className="p-12 br2 StatCard border border-solid mx-4 bg-white text-black text-center grow">
-      <CountUp
-        start={0}
-        end={props.amount}
-        duration={3}
-        separator=" "
-        decimals={props.decimal}
-        prefix={props.prefix}>
-        {({ countUpRef, start }) => (
-          <div>
-            <h1 className="f1 lh-title font-merriweather" ref={countUpRef} />
-            <Waypoint onEnter={()=>{
-              start();
-            }}>
-            </Waypoint>
-          </div>
-        )}
-      </CountUp>
+    <div className={props.className}>
+      <div className="p-12 br2 StatCard border border-solid mx-4 bg-white text-black text-center grow">
+        <CountUp
+          start={0}
+          end={props.amount}
+          duration={3}
+          separator=" "
+          decimals={props.decimal}
+          prefix={props.prefix}>
+          {({ countUpRef, start }) => (
+            <div>
+              <h1 className="f1 lh-title font-merriweather" ref={countUpRef} />
+              <Waypoint onEnter={() => {
+                start();
+              }}>
+              </Waypoint>
+            </div>
+          )}
+        </CountUp>
 
-      <h2 className="f2 lh-title font-opensans font-gray b">{props.name} </h2>
+        <h2 className="f2 lh-title font-opensans font-gray b">{props.name} </h2>
+      </div>
     </div>
   );
 }

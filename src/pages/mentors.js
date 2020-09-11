53e516f96117fe787a3cd2940f11e1f004a135e6
gargAbhinav4699 <34428698+gargAbhinav4699@users.noreply.github.com>
2020-09-10 23:55:23 -0400

    Mentors page

--- src/pages/mentors.js
+++ src/pages/mentors.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout/Layout";
 import SEO from "../components/SEO";
+import Card from '../components/Card';
 
 export default () => {
   return (
@@ -9,9 +10,16 @@ export default () => {
       <SEO />
       <section id="countdown" className="">
         <div className="container mx-auto mt-10">
-          <h2 className="text-3xl lg:text-5xl font-semibold text-center">
+          <h2 className="text-3xl lg:text-5xl font-semibold text-center title">
             Mentors
           </h2>
+        </div>
+        <div className="container mx-auto px-8 text-center">
+          <Card className="mx-4 bg-white text-black text-center">
+            <h1 className="text-2xl font-semibold">
+              Google Sheets Embed
+            </h1>
+          </Card>
         </div>
       </section>
     </Layout>

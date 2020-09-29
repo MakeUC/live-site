0d742613588e3471f3a9495544dcdfaded51826b
gargAbhinav4699 <34428698+gargAbhinav4699@users.noreply.github.com>
2020-09-28 21:20:17 -0400

    Added accordian

---
+++ src/components/Accordian/Accordian.js
@@ -0,0 +1,59 @@
+import React, { useState, useRef } from 'react';
+
+import './Accordian.scss';
+
+function Chevron(props) {
+  return (
+    <svg
+      className={props.className}
+      height={props.height}
+      width={props.width}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 320 512"
+    >
+      <path
+        fill={props.fill}
+        d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+      />
+    </svg>
+  );
+}
+
+function Accordion(props) {
+  const [setActive, setActiveState] = useState("");
+  const [setHeight, setHeightState] = useState("0px");
+  const [setRotate, setRotateState] = useState("accordion__icon");
+
+  const content = useRef(null);
+
+  function toggleAccordion() {
+    setActiveState(setActive === "" ? "active" : "");
+    setHeightState(
+      setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+    );
+    setRotateState(
+      setActive === "active" ? "accordion__icon" : "accordion__icon rotate"
+    );
+  }
+
+  return (
+    <div className="accordion__section">
+      <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
+        <p className="accordion__title">{props.title}</p>
+        <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
+      </button>
+      <div
+        ref={content}
+        style={{ maxHeight: `${setHeight}` }}
+        className="accordion__content"
+      >
+        <div
+          className="accordion__text"
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Accordion;
